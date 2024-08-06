0616f58f67f4b91af8b1eb4bb69ca1978b636bdf
biyash <bsstha21@gmail.com>
2024-08-06 22:45:35 +0545

    change

---
+++ backend/controllera/userController.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+      res.json({ msg: "success" });
+    } else {
+      res.json({ msg: "failed" });
+    }
+  });
