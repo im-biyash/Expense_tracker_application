171062326448fe2fd414613193a2235e8a8177c5
biyash <bsstha21@gmail.com>
2024-08-17 23:33:14 +0545

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Home/>
     </main>
   );
 }
