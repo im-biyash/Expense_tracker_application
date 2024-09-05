5fe435d9fb75940d45d71c97a7df2cf6bb9ffb3e
biyash <bsstha21@gmail.com>
2024-09-05 23:26:07 +0545

    change

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,22 +1,32 @@
+
+
+"use client"
 import React from 'react';
 import Image from 'next/image'; // Use 'next/image' for optimized images in Next.js
 import expense from '../assets/expense.jpg'
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 const Home = () => {
+const router = useRouter();
+
+  const handleStart = () =>{
+    router.push('/login')
+  }
   return (
     <div className='h-80vh flex flex-col justify-center items-center p-6'>
       <div className='flex flex-col md:flex-row items-center gap-6 justify-evenly w-full h-full'>
         {/* Left Side */}
         <div className='flex flex-col items-start max-w-lg text-center md:text-left mb-11'>
           <h1 className='text-4xl font-bold mb-4'>Expense Tracker Application</h1>
-          <p className='text-lg text-gray-700'>
+          <p className='text-lg text-gray-400'>
             Track your expenses in an efficient and organized way. 
             Our application helps you keep tabs on your spending, 
             set budgets, and make informed financial decisions. 
             With intuitive charts and easy-to-use features, 
             managing your finances has never been easier.
           </p>
-          <Button className='mt-4'>Get Started</Button>
+          <Button variant="pinkGradient" className='mt-6' onClick={handleStart}>Get Started</Button>
+
         </div>
 
         {/* Right Side */}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { ModeToggle } from "../components/Mode-toogle";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../app/store"; // Adjust the path accordingly
-import { login, logout } from ".././app/features/auth/authSlice";
+import { login, logout } from "../app/features/auth/authSlice";
 
 const Navbar = () => {
   const router = useRouter();
@@ -26,11 +26,9 @@ const Navbar = () => {
     const token = localStorage.getItem('token');
     if (token) {
       const decodedToken = parseJwt(token);
-      // Ensure the token is valid and contains the necessary information
       if (decodedToken && Date.now() < decodedToken.exp * 1000 && decodedToken.username && decodedToken.email) {
         dispatch(login({ username: decodedToken.username, email: decodedToken.email, token }));
       } else {
-        // Token is invalid, missing information, or expired
         localStorage.removeItem('token');
         dispatch(logout());
       }
@@ -48,16 +46,27 @@ const Navbar = () => {
   };
 
   return (
-    <div className="my-2 p-2">
+    <div className=" p-2 bg-gray-100 dark:bg-gray-900 shadow">
       <nav className="flex justify-between items-center">
-        <ul className="flex flex-grow justify-center gap-2">
+        <ul className="flex flex-grow justify-center gap-6 font-serif ">
           <li>
             <Link href="/">Home</Link>
           </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/developer">Developer</Link>
+          </li>
           {isLoggedIn && (
-            <li>
-              <Link href="/dashboard">Dashboard</Link>
-            </li>
+            <>
+              <li>
+                <Link href="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link href="/transcationlogs">Transcation logs</Link>
+              </li>
+            </>
           )}
         </ul>
         <div className="flex gap-2 p-2 mr-3">
