cec37f21ae5c00a3eb269000e47c90b504b50ebc
biyash <bsstha21@gmail.com>
2024-09-05 22:02:51 +0545

    home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,39 @@
-'use client';
-import React, { useEffect } from 'react';
-import Link from 'next/link';
-import { Button } from '../components/ui/button';
-import { ModeToggle } from '../components/Mode-toogle';
-import { useRouter } from 'next/navigation';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../app/store'; // Adjust the path accordingly
-import { login, logout } from '.././app/features/auth/authSlice';
+"use client";
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { Button } from "../components/ui/button";
+import { ModeToggle } from "../components/Mode-toogle";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../app/store"; // Adjust the path accordingly
+import { login, logout } from ".././app/features/auth/authSlice";
 
 const Navbar = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
+  const parseJwt = (token: string) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+      console.error("Invalid token:", e);
+      return null;
+    }
+  };
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      dispatch(login({token}));
+      const decodedToken = parseJwt(token);
+      // Ensure the token is valid and contains the necessary information
+      if (decodedToken && Date.now() < decodedToken.exp * 1000 && decodedToken.username && decodedToken.email) {
+        dispatch(login({ username: decodedToken.username, email: decodedToken.email, token }));
+      } else {
+        // Token is invalid, missing information, or expired
+        localStorage.removeItem('token');
+        dispatch(logout());
+      }
     }
   }, [dispatch]);
 
